5fa078ead7fbac8b87b9b3414b2fbc8ec52eb4a1
Linus Norton <linusnorton@gmail.com>
2016-06-23 20:43:22 +0100

    date fix

--- src/utils/DateTimeHelper.js
+++ src/utils/DateTimeHelper.js
@@ -9,7 +9,11 @@ export default class DateTimeHelper {
     const d1 = moment(date1, appConfig.serverTimeFormat);
     const d2 = moment(date2, appConfig.serverTimeFormat);
 
-    return d2.subtract(d1).format(appConfig.uiDurationFormat);
+    if (d1.isAfter(d2)) {
+      d2.add(1, "days");
+    }
+
+    return moment.duration(d2.diff(d1)).format(appConfig.uiDurationFormat);
   }
 
   static parseDuration(str) {
