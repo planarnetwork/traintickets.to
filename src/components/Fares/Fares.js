202f5142df875532e2f6dea34a1719ce1ae77e57
Linus Norton <linusnorton@gmail.com>
2017-11-08 15:54:37 +0000

    performance improvements

--- src/components/Fares/Fares.js
+++ src/components/Fares/Fares.js
@@ -5,6 +5,17 @@ import locations from '../../data/locations.json';
 
 import './Fares.css';
 
+const options = {
+  items: 8,
+  nav: true,
+  rewind: false,
+  autoplay: false,
+  autoWidth: false,
+  margin: 20,
+  mouseDrag: false,
+  navText: ['<i class="fa fa-caret-left" aria-hidden="true"></i>', '<i class="fa fa-caret-right" aria-hidden="true"></i>'],
+};
+
 class Fares extends Component {
     constructor(props) {
         super(props);
@@ -42,17 +53,6 @@ class Fares extends Component {
 
     render() {
         let tap = Number(this.state.inwardPrice * 100  + Number(this.state.outwardPrice * 100)) / 100;
-
-        const options = {
-            items: 8,
-            nav: true,
-            rewind: false,
-            autoplay: false,
-            autoWidth: false,
-            margin: 20,
-            mouseDrag: false,
-            navText: ['<i class="fa fa-caret-left" aria-hidden="true"></i>', '<i class="fa fa-caret-right" aria-hidden="true"></i>'],
-        };
         let originFound = locations.find((e) => {
             return this.props.searchResult.response ? e.code === this.props.searchResult.response.inward.map((key) => key.origin)[0] : undefined;
         });
@@ -71,122 +71,14 @@ class Fares extends Component {
                     <div className="fares-out">
                         <h3 className="fares-title bold">{origin ? 'OUTBOUND - ' + origin : ''}{destination ? ' to ' + destination : ''}</h3>
                         <ul className="fare-list clearfix">
-                            {this.props.searchResult.response.length ? '0 Result' : ''}
-                            <OwlCarousel ref="fares-out" options={options}>
-                                {this.props.searchResult.response.inward.map((key, ind) => {
-                                    let price, result;
-                                    let firstDate = key.departureTime;
-                                    let secondDate = key.arrivalTime;
-                                    let getDate = (string) => new Date(0, 0,0, string.split(':')[0], string.split(':')[1]);
-                                    let different = (getDate(secondDate) - getDate(firstDate));
-                                    let differentRes, hours, minuts;
-                                    if(different > 0) {
-                                        differentRes = different;
-                                        hours = Math.floor((differentRes % 86400000) / 3600000);
-                                        minuts = Math.round(((differentRes % 86400000) % 3600000) / 60000);
-                                    } else {
-                                        differentRes = Math.abs((getDate(firstDate) - getDate(secondDate)));
-                                        hours = Math.floor(24 - (differentRes % 86400000) / 3600000);
-                                        minuts = Math.round(60 - ((differentRes % 86400000) % 3600000) / 60000);
-                                    }
-                                    result = hours + ':' + minuts;
-
-                                    this.props.searchResult.response.outward.map((out) => {
-                                        let temp = Object.keys(this.props.searchResult.response.fares).filter((k) => {
-                                            return (k.indexOf(out.id) > -1) && (k.indexOf(key.id) > -1)
-                                        }).map((key) => key);
-                                        let value = this.props.searchResult.response.fares[temp];
-                                        price = this.props.searchResult.links[value[0]];
-                                        price = price.totalPrice / 100;
-                                    });
-                                    console.log('Inward - ' + key.id + ': ---------------------------------- PRICE: ' + price);
-
-                                    return (
-                                        <Paper key={ind} className="fare pull-left" zDepth={2}>
-                                            <label className="fare-input center">
-                                                <div className="fare-stations bold clearfix">
-                                                    <span className="fare-station pull-left">{key.origin}</span>
-                                                    <span className="fare-station pull-right">{key.destination}</span>
-                                                </div>
-                                                <div className="fare-times bold clearfix">
-                                                    <time className="fare-time pull-left">{key.departureTime}</time>
-                                                    <time className="fare-time pull-right">{key.arrivalTime}</time>
-                                                </div>
-                                                <p className="duration">{result} min</p>
-                                                <p className="fare-ticket">Anytime Day Single with Child Flat Fare Single</p>
-                                                <div className="fare-bottom">
-                                                    <p className="fare-price bold">
-                                                        <span className="pound">&#163;</span>{price ? price.toString().match(/^\d+/g) : ''}<span className="pence">{price ? price.toString().match(/\.\d+/g) : ''}</span>
-                                                    </p>
-                                                    <input type="radio" className="radio" name="out" onClick={() => this.handleInwardPrice(price)} />
-                                                    <span className="radio-custom"></span>
-                                                </div>
-                                            </label>
-                                        </Paper>
-                                    )
-                                })}
-                            </OwlCarousel>
+                            {this.props.searchResult.response.length ? 'No results' : this.getCarosel(this.props.searchResult.response.outward)}
                         </ul>
                     </div>
                     <div className="fares-return">
                         <h3 className="fares-title bold">{destination ? 'RETURN - ' + destination : ''}{origin ? ' to ' + origin: ''}</h3>
                         <ul className="fare-list clearfix">
-                            {this.props.searchResult.response.length ? '0 Result' : ''}
-                            <OwlCarousel ref="fares-out" options={options} >
-                                {this.props.searchResult.response.outward.map((key, ind) => {
-                                    let price, result;
+                            {this.props.searchResult.response.length ? 'No results' : this.getCarosel(this.props.searchResult.response.inward)}
 
-                                    let firstDate = key.departureTime;
-                                    let secondDate = key.arrivalTime;
-                                    let getDate = (string) => new Date(0, 0,0, string.split(':')[0], string.split(':')[1]);
-                                    let different = (getDate(secondDate) - getDate(firstDate));
-                                    let differentRes, hours, minuts;
-                                    if(different > 0) {
-                                        differentRes = different;
-                                        hours = Math.floor((differentRes % 86400000) / 3600000);
-                                        minuts = Math.round(((differentRes % 86400000) % 3600000) / 60000);
-                                    } else {
-                                        differentRes = Math.abs((getDate(firstDate) - getDate(secondDate)));
-                                        hours = Math.floor(24 - (differentRes % 86400000) / 3600000);
-                                        minuts = Math.round(60 - ((differentRes % 86400000) % 3600000) / 60000);
-                                    }
-                                    result = hours + ':' + minuts;
-
-                                    this.props.searchResult.response.inward.map((inw) => {
-                                        let temp = Object.keys(this.props.searchResult.response.fares).filter((k) => {
-                                            return (k.indexOf(inw.id) > -1) && (k.indexOf(key.id) > -1)
-                                        }).map((key) => key);
-                                        let value = this.props.searchResult.response.fares[temp];
-                                        price = this.props.searchResult.links[value[1] || value[0]];
-                                        price = price.totalPrice / 100;
-                                    });
-                                    console.log('Outward - ' + key.id + ': ---------------------------------- PRICE: ' + price);
-
-                                    return (
-                                        <Paper key={ind} className="fare pull-left" zDepth={2}>
-                                            <label className="fare-input center" onClick={() => this.handleOutwardPrice(price)}>
-                                                <div className="fare-stations bold clearfix">
-                                                    <span className="fare-station pull-left">{key.origin}</span>
-                                                    <span className="fare-station pull-right">{key.destination}</span>
-                                                </div>
-                                                <div className="fare-times bold clearfix">
-                                                    <time className="fare-time pull-left">{key.departureTime}</time>
-                                                    <time className="fare-time pull-right">{key.arrivalTime}</time>
-                                                </div>
-                                                <p className="duration">{result} min</p>
-                                                <p className="fare-ticket">Anytime Day Single with Child Flat Fare Single</p>
-                                                <div className="fare-bottom">
-                                                    <p className="fare-price bold">
-                                                        <span className="pound">&#163;</span>{price ? price.toString().match(/^\d+/g) : ''}<span className="pence">{price ? price.toString().match(/\.\d+/g) : ''}</span>
-                                                    </p>
-                                                    <input type="radio" className="radio" name="return"/>
-                                                    <span className="radio-custom"></span>
-                                                </div>
-                                            </label>
-                                        </Paper>
-                                    )
-                                })}
-                            </OwlCarousel>
                         </ul>
                     </div>
                 </div>
@@ -202,6 +94,66 @@ class Fares extends Component {
                 ) : []}
             </section>
         );
+    }
+
+    getCarosel(journeys) {
+      return (<OwlCarousel ref="fares-out" options={options}>
+        {journeys.map((journey, ind) => {
+          const firstDate = new Date("01/01/1970 " + journey.departureTime);
+          const secondDate = new Date("01/01/1970 " + journey.arrivalTime);
+          const difference = secondDate.getTime() - firstDate.getTime();
+          const hours = Math.floor(difference / 3600000);
+          const minutes = Math.floor(difference % 3600000 / 60000);
+          const time = hours + ':' + minutes;
+          const cheapestFareOption = this.getCheapestFareOptionFor(journey.id);
+          const price = cheapestFareOption.totalPrice; //todo: blank return prices for inward journeys
+
+          return (
+            <Paper key={ind} className="fare pull-left" zDepth={2}>
+              <label className="fare-input center">
+                <div className="fare-stations bold clearfix">
+                  <span className="fare-station pull-left">{journey.origin}</span>
+                  <span className="fare-station pull-right">{journey.destination}</span>
+                </div>
+                <div className="fare-times bold clearfix">
+                  <time className="fare-time pull-left">{journey.departureTime}</time>
+                  <time className="fare-time pull-right">{journey.arrivalTime}</time>
+                </div>
+                <p className="duration">{time} min</p>
+                <p className="fare-ticket">Ticket type</p>
+                <div className="fare-bottom">
+                  <p className="fare-price bold">
+                    <span className="pound">&#163;</span>{Math.floor(price / 100)}<span className="pence">.{(price % 100)}</span>
+                  </p>
+                  <input type="radio" className="radio" name="out" onClick={() => this.handleInwardPrice(price)} />
+                  <span className="radio-custom"></span>
+                </div>
+              </label>
+            </Paper>
+          )
+        })}
+      </OwlCarousel>);
+    }
+
+    getCheapestFareOptionFor(journeyId) {
+      let price = Number.MAX_SAFE_INTEGER;
+      let cheapestFareOption;
+
+      for (const journeys in this.props.searchResult.response.fares) {
+        const indexOfJourney = journeys.indexOf(journeyId);
+        if (indexOfJourney !== -1) {
+          const fareIndex = indexOfJourney > 0 ? this.props.searchResult.response.fares[journeys].length -1 : 0;
+          const fareOptionId = this.props.searchResult.response.fares[journeys][fareIndex];
+          const fareOption = this.props.searchResult.links[fareOptionId];
+
+          if (fareOption.totalPrice < price) {
+            price = fareOption.totalPrice;
+            cheapestFareOption = fareOption;
+          }
+        }
+      }
+
+      return cheapestFareOption;
     }
 }
 
