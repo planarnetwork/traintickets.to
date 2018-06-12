451f421153dfaba7b5e74956e7ae88cf4c8b3b88
perryharlock <perry@phwebs.co.uk>
2018-06-12 10:34:56 +0100

    Amend layout of form search

--- src/component/Search/Search.tsx
+++ src/component/Search/Search.tsx
@@ -54,94 +54,95 @@ export class Search extends React.Component<SearchProps, SearchState> {
 
   public render() {
     return (
-      <section className="search">
-        <div className="container">
-          <form className="row">
-            <div className="search--column col-md-7 col-lg-5">
-              <div className="row">
-                <div className="col-sm-12 col-md-24">
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="origin">From</label>
-                    <StationInput name="origin" defaultValue={"geo"} placeholder="Leaving from" onChange={this.set}/>
-                  </div>
-                </div>
-                <div className="col-sm-12 col-md-24">
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="destination">Going to</label>
-                    <StationInput name="destination" defaultValue={this.state.destination} placeholder="Going to" onChange={this.set}/>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="search--column col-md-7 col-lg-4 col-xl-3">
-              <div className="row">
-                <div className="col-12 col-md-24">
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="outDate">Outward Date</label>
-                    <DatePicker
-                      onChange={this.onOutwardDateChange}
-                      minDate={moment()}
-                      selected={moment(this.state.outwardDate)}
-                      dateFormat="ddd, DD MMM YYYY"
-                      placeholderText={"Outward date"}
-                      className="form--input"
-                    />
-                  </div>
-                </div>
-                <div className="col-12 col-md-24">
-                  <div className="form-group">
-                    <label className="form-label" htmlFor="retDate">Return Date</label>
-                    <DatePicker
-                      onChange={this.onReturnDateChange}
-                      minDate={moment(this.state.outwardDate)}
-                      selected={this.state.returnDate ? moment(this.state.returnDate!) : null}
-                      dateFormat="ddd, DD MMM YYYY"
-                      isClearable={true}
-                      placeholderText={"Return date"}
-                      className="form--input"
-                    />
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-            <div className="search--column col-12 col-sm-12 col-md-4 col-lg-2">
-              <div className="row">
-                <div className="col-12 col-md-24">
-                  <NumberInput name="adults" label="Adults" min={0} max={9} defaultValue={this.state.adults} onChange={this.set}/>
-                </div>
-                <div className="col-12 col-md-24">
-                  <NumberInput name="children" label="Children" min={0} max={9} defaultValue={this.state.children} onChange={this.set}/>
-                </div>
-              </div>
-            </div>
-
-            <div className="search--column col-12 col-sm-12 col-md-6 col-lg-4 col-xl-5">
-              <div className="form-group">
-                <label className="form-label" htmlFor="child">Railcards</label>
-                <RailcardSelect name="railcards" max={4} onChange={this.set}/>
-              </div>
-            </div>
-
-            <div className="col-md-24 col-lg">
-              <div className="search--filters">
-                <legend className="form-label">Filters</legend>
+      <form>
+        <div className="search">
+          <div className="container">
+            <div className="row">
+              <div className="search--column col-md-12 col-lg-8">
                 <div className="row">
-                  <div className="col">
-                    <RadioGroup name="class" options={["standardClass", "firstClass"]} labels={["Standard", "First"]} onChange={this.set}/>
+                  <div className="col-sm-12">
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="origin">From</label>
+                      <StationInput name="origin" defaultValue={"geo"} placeholder="Leaving from" onChange={this.set}/>
+                    </div>
                   </div>
-                  <div className="col">
-                    <Checkbox label="Singles" name="singles" checked={this.state.singles} onChange={this.set}/>
-                    <Checkbox label="Returns" name="returns" checked={this.state.returns} onChange={this.set}/>
+                  <div className="col-sm-12">
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="destination">Going to</label>
+                      <StationInput name="destination" defaultValue={this.state.destination} placeholder="Going to" onChange={this.set}/>
+                    </div>
                   </div>
                 </div>
               </div>
+
+              <div className="search--column col-md-12 col-lg-7">
+                <div className="row">
+                  <div className="col-12">
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="outDate">Outward Date</label>
+                      <DatePicker
+                        onChange={this.onOutwardDateChange}
+                        minDate={moment()}
+                        selected={moment(this.state.outwardDate)}
+                        dateFormat="ddd, DD MMM YYYY"
+                        placeholderText={"Outward date"}
+                        className="form--input"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-group">
+                      <label className="form-label" htmlFor="retDate">Return Date</label>
+                      <DatePicker
+                        onChange={this.onReturnDateChange}
+                        minDate={moment(this.state.outwardDate)}
+                        selected={this.state.returnDate ? moment(this.state.returnDate!) : null}
+                        dateFormat="ddd, DD MMM YYYY"
+                        isClearable={true}
+                        placeholderText={"Return date"}
+                        className="form--input"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="search--column col-12 col-md-6 col-lg-4">
+                <div className="row">
+                  <div className="col-12">
+                    <NumberInput name="adults" label="Adults" min={0} max={9} defaultValue={this.state.adults} onChange={this.set}/>
+                  </div>
+                  <div className="col-12">
+                    <NumberInput name="children" label="Children" min={0} max={9} defaultValue={this.state.children} onChange={this.set}/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="search--column col-12 col-md-8 col-lg-5">
+                <div className="form-group">
+                  <label className="form-label" htmlFor="child">Railcards</label>
+                  <RailcardSelect name="railcards" max={4} onChange={this.set}/>
+                </div>
+              </div>
             </div>
-          </form>
+          </div>
         </div>
-      </section>
+        <div className="search--filters">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 col-md-8 col-lg-6 col-xl-5">
+                <legend className="form-label">Class</legend>
+                <RadioGroup name="class" options={["standardClass", "firstClass"]} labels={["Standard", "First"]} onChange={this.set}/>
+              </div>
+              <div className="col-sm-12 col-md-8 col-lg-6 col-xl-5">
+                <legend className="form-label">Ticket type</legend>
+                <Checkbox label="Singles" name="singles" checked={this.state.singles} onChange={this.set}/>
+                <Checkbox label="Returns" name="returns" checked={this.state.returns} onChange={this.set}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
 }
