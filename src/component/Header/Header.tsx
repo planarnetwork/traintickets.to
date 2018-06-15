62df26b8640c9f60affe634a6d0b2b68a1bf0d5f
Linus Norton <linusnorton@gmail.com>
2018-06-15 11:39:36 +0100

    remove strapline

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -5,7 +5,6 @@ export function Header() {
   return (
     <header className="header clearfix">
       <h1 className="header--logo">train<span className='color-highlight'>tickets</span>.to</h1>
-      <h2 className="header--strapline">the <span className="color-highlight">cheapest</span> fare finder</h2>
     </header>
   )
 }
