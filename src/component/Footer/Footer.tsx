d9ea94b6fbad77776fd09435977dbd0cd25a4be7
perryharlock <perry@phwebs.co.uk>
2018-06-28 13:57:58 +0100

    Add in basic footer modal

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
 import {Price} from "./../Price/Price";
 import './Footer.css';
+import './Modal.css';
 import autobind from "autobind-decorator";
 
 @autobind
 export class Footer extends React.Component<FooterProps, FooterState> {
 
   public state = {
-    open: true
+    popupOpen: true,
+    modalOpen: false
   };
 
-  public onClose() {
-    this.setState({ open: false });
+  public closePopup() {
+    this.setState({ popupOpen: false });
+  }
+
+  public openModal() {
+    this.setState({ modalOpen: true });
+  }
+
+  public closeModal() {
+    this.setState({ modalOpen: false });
   }
 
   public render() {
@@ -25,6 +35,7 @@ export class Footer extends React.Component<FooterProps, FooterState> {
               </p>
             </div>
             <div className="col text-right">
+              <button onClick={this.openModal}>Open modal</button>
               <div className="footer--total">
                 <span className="footer--price">
                   Total <Price value={this.props.price}/>
@@ -33,7 +44,10 @@ export class Footer extends React.Component<FooterProps, FooterState> {
               </div>
             </div>
           </div>
-          { this.state.open && this.renderPopup() }
+          { this.state.popupOpen && this.renderPopup() }
+        </div>
+        <div className={this.state.modalOpen ? 'modal--bg is-active' : 'modal--bg'}>
+          {this.state.modalOpen && this.renderModal() }
         </div>
       </footer>
     );
@@ -48,9 +62,45 @@ export class Footer extends React.Component<FooterProps, FooterState> {
         <p className="footer-pop--message">
           The total of your selected tickets for <span className="bold">ALL</span> passengers is shown below.
         </p>
-        <button type="button" className="footer-pop--close" onClick={this.onClose}>
+        <button type="button" className="footer-pop--close" onClick={this.closePopup}>
           <span className="sr-only">close</span>
         </button>
+      </div>
+    )
+  }
+
+  public renderModal() {
+    return (
+      <div aria-modal={true}>
+        <div className="modal--content" role="dialog" aria-label="Ticket modal">
+          <div className="modal--header">
+            <button type="button" className="modal--btn__close" onClick={this.closeModal}>Close</button>
+            <h4 className="modal--title">Title</h4>
+          </div>
+          <div className="modal--body">
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+            <p>This is some text for the body</p>
+          </div>
+          <div className="modal--footer">
+            <button
+              type="button"
+              className="modal--btn__footer"
+              onClick={this.closeModal}
+            >
+            Close
+            </button>
+          </div>
+        </div>
       </div>
     )
   }
@@ -61,5 +111,6 @@ export interface FooterProps {
 }
 
 interface FooterState {
-  open: boolean;
+  popupOpen: boolean;
+  modalOpen: boolean;
 }
