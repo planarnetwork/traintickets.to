2ad0ae4f2f0e658b49ac0c25ee2d860b951cbfe0
perryharlock <perry@phwebs.co.uk>
2018-06-29 16:57:08 +0100

    Bit of styling to start with and order the info

--- src/component/Footer/FareInformation/FareInformation.tsx
+++ src/component/Footer/FareInformation/FareInformation.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import {Price} from "../../Price/Price";
 import {FareUse} from "../../../service/JourneyPlanner/JourneyPlanner";
 import autobind from "autobind-decorator";
+import './FareInfo.css';
 
 @autobind
 export class FareInformation extends React.Component<FareInformationProps> {
@@ -19,18 +20,34 @@ export class FareInformation extends React.Component<FareInformationProps> {
 
     return (
       <React.Fragment key={id}>
-        <h4>Tickets</h4>
-        <ul>
-          { fareOption.fares.map(this.renderTicketPrice) }
-        </ul>
-        <p>Total: <Price value={fareOption.totalPrice} /></p>
-        <h4>Ticket Information</h4>
-        <p>Origin: {origin.name}</p>
-        <p>Destination: {destination.name}</p>
-        <p>Route: {route.name} ({route.code})</p>
-        <p>Ticket type: {ticketType.name} ({ticketType.code})</p>
-        <p>Valid for outward for 1 day, return within 1 month (TODO)</p>
-        {fare.restriction && (<a target="_blank" href={"http://www.nationalrail.co.uk/" + fare.restriction}>Restrictions apply</a>)}
+        <div className="row">
+          <div className="col-md-18 offset-md-3">
+            <div className="fare-info">
+              <h4 className="fare-info--header">{ticketType.name} ({ticketType.code})</h4>
+              <div className="fare-info--body">
+                <p>From <br/>{origin.name}</p>
+                <p>To <br/>{destination.name}</p>
+                <p>Route: {route.name} ({route.code})</p>
+                <p>Valid for outward for 1 day, return within 1 month (TODO)</p>
+                {fare.restriction && (<a target="_blank" href={"http://www.nationalrail.co.uk/" + fare.restriction}>Restrictions apply</a>)}
+              </div>
+              <div className="fare-info--footer">
+                <p>Total: <Price value={fareOption.totalPrice} /></p>
+              </div>
+            </div>
+            <div className="fare-info">
+              <h4 className="fare-info--header">Price breakdown</h4>
+              <div className="fare-info--body">
+                <ul>
+                  { fareOption.fares.map(this.renderTicketPrice) }
+                </ul>
+              </div>
+              <div className="fare-info--footer">
+                <p>Total: <Price value={fareOption.totalPrice} /></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
@@ -44,7 +61,7 @@ export class FareInformation extends React.Component<FareInformationProps> {
 
     return (
       <li className="clearfix" key={index}>
-        {adults}{comma}{children} {railcard}<span className="pull-right"> @<Price value={fare.price} /></span>
+        {adults}{comma}{children} {railcard}<span className="pull-right"> at<Price value={fare.price} /></span>
       </li>
     )
   }
