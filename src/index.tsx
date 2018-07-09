7953e7335eec7691302d06bb13d7ddca44456aa9
Linus Norton <linusnorton@gmail.com>
2018-07-09 08:09:21 +0100

    remove service worker caching

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import {IndexPage} from './page/Index/IndexPage';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 ReactDOM.render(
   <IndexPage />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+unregister();
