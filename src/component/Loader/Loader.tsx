de8c4dffca18e206067263cfbe911f232ecbab95
Linus Norton <linusnorton@gmail.com>
2018-07-20 14:14:48 +0100

    tidy up

--- src/component/Loader/Loader.tsx
+++ src/component/Loader/Loader.tsx
@@ -2,21 +2,20 @@ import * as React from 'react';
 import './Loader.css';
 
 export class Loader extends React.Component<LoaderProps> {
-  public render() {
-    const { text } = this.props;
 
+  public render() {
     return (
-        <div className="loader-container">
-            <div className="loader">
-                <div className="loader-cube" />
-                <div className="loader-cube loader-cube2" />
-                <div className="loader-cube loader-cube3" />
-                <div className="loader-cube loader-cube4" />
-            </div>
-            <p className="loader-text">{text}</p>
+      <div className="loader-container">
+        <div className="loader">
+          <div className="loader-cube" />
+          <div className="loader-cube loader-cube2" />
+          <div className="loader-cube loader-cube3" />
+          <div className="loader-cube loader-cube4" />
         </div>
-      )
-    }
+        <p className="loader-text">{this.props.text}</p>
+      </div>
+    )
+  }
 }
 
 interface LoaderProps {
