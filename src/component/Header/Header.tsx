51c6b786278340d5f0027aad4101d7e0712aa893
Linus Norton <linusnorton@gmail.com>
2018-07-27 10:32:55 +0100

    only show quick links on homepage

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -3,15 +3,27 @@ import {Nav} from "./../Nav/Nav";
 import {QuickLinks} from "./../QuickLinks/QuickLinks";
 import './Header.css';
 import {Link} from "react-router-dom";
+import {withRouter} from 'react-router-dom';
 
-export function Header() {
-  return (
-    <header className="header clearfix">
-      <h1 className="header--logo">
-        <Link className="header--link" to="/">train<span className='color-highlight'>tickets</span>.to</Link>
-      </h1>
-      <Nav />
-      <QuickLinks />
-    </header>
-  )
+class HeaderComponent extends React.Component<HeaderProps> {
+
+  public render() {
+    return (
+      <header className="header clearfix">
+        <h1 className="header--logo">
+          <Link className="header--link" to="/">train<span className='color-highlight'>tickets</span>.to</Link>
+        </h1>
+        <Nav />
+        { this.props.location.pathname === "/" && <QuickLinks /> }
+      </header>
+    )
+  };
+}
+
+export const Header = withRouter((props: HeaderProps) => <HeaderComponent {...props}/>);
+
+interface HeaderProps {
+  location: {
+    pathname: string;
+  };
 }
